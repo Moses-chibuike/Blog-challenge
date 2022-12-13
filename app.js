af920922700c055f5489d93122c01033899f7d98
zeusmoney <ezechukwuchibuikemoses@gmail.com>
2022-12-13 17:22:45 +0000

    more commit

--- app.js
+++ app.js
@@ -23,7 +23,18 @@ app.get("/", function(req, res){
 });
 
 
+app.get("/about", function(req, res){
+  res.render("about", {aboutContent: aboutContent});
+});
 
+app.get("/contact", function(req, res){
+  res.render("contact", {contactContent: contactContent});
+});
+
+
+app.get("/compose", function(req, res){
+  res.render("compose");
+});
 
 
 
