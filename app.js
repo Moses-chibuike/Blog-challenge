9400f294216b7a1ca2e45083c380e22e763735ed
zeusmoney <ezechukwuchibuikemoses@gmail.com>
2022-12-15 14:48:02 +0000

    more additions

--- app.js
+++ app.js
@@ -15,10 +15,12 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+let posts = [];
 
 app.get("/", function(req, res){
   res.render("home", {
     startingContent: homeStartingContent,
+    posts: posts
     });
 });
 
